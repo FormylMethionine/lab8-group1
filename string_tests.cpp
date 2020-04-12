8fbc590e91a15b5db25118d4609eb6e28afed02d
Vincent Le Goff <vb.legoff@free.fr>
2020-04-12 15:13:32 +0200

    Added test for int size() method

--- string_tests.cpp
+++ string_tests.cpp
@@ -10,10 +10,24 @@ bool test_c_str(string& test){
 	}
 }
 
+bool test_size(string& test, int expected_size){
+	int value = test.size();
+	if (value == expected_size){
+		return true;
+	} else {
+		return false;
+	}
+}
+
 int main(){
 	string test_cstring_constructor("Hello World"); 
 	string test_copy_constructor(test_cstring_constructor); 
+
 	std::cout << test_c_str(test_cstring_constructor) << std::endl;
 	std::cout << test_c_str(test_copy_constructor) << std::endl;
+	
+	std::cout << test_size(test_cstring_constructor, 11) << std::endl;
+	std::cout << test_size(test_copy_constructor, 11) << std::endl;
+
 	return 0;
 }
