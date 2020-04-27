469d278e6674e91b5985996008e8197a21c6648c
Vincent Le Goff <vb.legoff@free.fr>
2020-04-27 02:34:43 +0200

    Clarified test messages

--- string_tests.cpp
+++ string_tests.cpp
@@ -8,32 +8,46 @@ int main(){
 	string test_clear_str("Hello World"); 
 	string test_eq_char_str("Hello World");
 
-	std::cout << test_c_str(test_cstring_constructor) << std::endl;
-	std::cout << test_c_str(test_copy_constructor) << std::endl;
+	std::cout << "string can't exceed the maximal size: "
+		<< test_maxsize() << std::endl;
+
+	std::cout << "test for cstring constructor: "
+		<< test_c_str(test_cstring_constructor) << std::endl;
+
+	std::cout << "test for copy constructor: "
+		<< test_c_str(test_copy_constructor) << std::endl;
 	
-	std::cout << test_size(test_cstring_constructor, 11) << std::endl;
-	std::cout << test_size(test_copy_constructor, 11) << std::endl;
+	std::cout << "test for size_t size() const method: "
+		<< test_size(test_cstring_constructor, 11) << std::endl;
 
-	std::cout << test_clear(test_clear_str) << std::endl;
+	std::cout << "test for void clear() method: "
+		<< test_clear(test_clear_str) << std::endl;
 	
-	std::cout << test_eq_char(test_eq_char_str, 'c') << std::endl;
-	std::cout << test_eq_char(test_clear_str, 'c') << std::endl;
+	std::cout << "test for string& operator =(const char): "
+		<< test_eq_char(test_eq_char_str, 'c') << std::endl;
+	//std::cout << test_eq_char(test_clear_str, 'c') << std::endl;
+	//second test need to be combined in first
 
-	std::cout << test_addition() << std::endl;
+	std::cout << "test for string operator +(const string&, const char*): "
+		<< test_addition() << std::endl;
 
-	std::cout << test_length() << std::endl;
+	std::cout << "test for size_t length() const: "
+		<< test_length() << std::endl;
 
-	std::cout << test_maxsize() << std::endl;
+	std::cout << "test for size_t capacity() const: " 
+		<< test_capacity() << std::endl;
 
-	std::cout << test_capacity() << std::endl;
+	std::cout << "test for void empty(): "
+		<< test_empty() << std::endl;
 
-	std::cout << test_empty() << std::endl;
+	std::cout << "test for void reserve(size_t): " 
+		<< test_reserve() << std::endl;
 
-	std::cout << test_reserve() << std::endl;
+	std::cout << "test for string operator =(const char*): "
+		<< test_eq_cstr() << std::endl;
 
-	std::cout << test_eq_cstr() << std::endl;
-
-	std::cout << test_plus_2str() << std::endl;
+	std::cout << "test for string operator =(const char*, const char*): "
+		<< test_plus_2str() << std::endl;
 
 	return 0;
 }
